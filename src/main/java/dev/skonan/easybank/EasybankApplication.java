4ee22d915be161973b16e1980f8cc1e60e89830c
sergeemmanuelk <serge_em@outlook.fr>
2024-04-13 09:58:10 +0000

    initial commit

---
+++ src/main/java/dev/skonan/easybank/EasybankApplication.java
@@ -0,0 +1,13 @@
+package dev.skonan.easybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasybankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasybankApplication.class, args);
+	}
+
+}
