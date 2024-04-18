2474720ba67e8e164c36aff706e34f09bb55177b
sergeemmanuelk <serge_em@outlook.fr>
2024-04-18 15:11:46 +0000

    optimize import

--- src/main/java/dev/skonan/easybank/ObjectsValidator.java
+++ src/main/java/dev/skonan/easybank/ObjectsValidator.java
@@ -1,2 +1,26 @@
-package dev.skonan.easybank;public class ObjectsValidator {
+package dev.skonan.easybank;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+public class ObjectsValidator<T> {
+    private final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    private final Validator validator = factory.getValidator();
+
+    public void validate(T objectToValidate) {
+        Set<ConstraintViolation<T>> violations = validator.validate(objectToValidate);
+        if (!violations.isEmpty()) {
+            Set<String> errorMessages = violations.stream()
+                    .map(ConstraintViolation::getMessage)
+                    .collect(Collectors.toSet());
+            //throw new ObjectValidationException(errorMessages, objectToValidate.getClass().getName());
+        }
+    }
 }
--- src/main/java/dev/skonan/easybank/dto/UserDto.java
+++ src/main/java/dev/skonan/easybank/dto/UserDto.java
@@ -1,6 +1,7 @@
 package dev.skonan.easybank.dto;
 
 import dev.skonan.easybank.models.User;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,9 +13,27 @@ import lombok.Setter;
 @Builder
 public class UserDto {
     private Integer id;
+
+    @NotNull(message = "First name is required")
+    @NotEmpty(message = "First name is required")
+    @NotBlank(message = "First name is required")
     private String firstName;
+
+    @NotNull(message = "Last name is required")
+    @NotEmpty(message = "Last name is required")
+    @NotBlank(message = "Last name is required")
     private String lastName;
+
+    @NotNull(message = "Email is required")
+    @NotEmpty(message = "Email is required")
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email is invalid")
     private String email;
+
+    @NotNull(message = "Password is required")
+    @NotEmpty(message = "Password is required")
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 16, message = "The password must contain between 8 and 16 characters")
     private String password;
 
     public static UserDto fromEntity(User user) {
