8b674be0c94118c0361141b234a303535c68eaa1
sergeemmanuelk <serge_em@outlook.fr>
2024-04-27 15:53:50 +0000

    feat: implement the global exception handler

---
+++ src/main/java/dev/skonan/easybank/handlers/ExceptionRepresentation.java
@@ -0,0 +1,19 @@
+package dev.skonan.easybank.handlers;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+import java.util.Set;
+
+@Builder
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ExceptionRepresentation {
+
+    private String errorMessage;
+
+    private String errorSource;
+
+    private Set<String> validationErrors;
+}
